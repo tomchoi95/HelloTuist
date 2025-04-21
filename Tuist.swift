11fe26db914e5d33b02869b3afa880630e144803
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-21 10:49:34 +0900

    타겟을 테스트용과 앱용으로 분리.

--- Tuist.swift
+++ Tuist.swift
@@ -1,3 +1,6 @@
 import ProjectDescription
 
 let tuist = Tuist(project: .tuist())
+
+// 장점. 협업
+// 빌드 빨리할 수 있음.
--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -7,8 +7,7 @@ import PackageDescription
     let packageSettings = PackageSettings(
         // Customize the product types for specific package product
         // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,]
-        productTypes: [:]
+        productTypes: ["Alamofire": .framework]
     )
 #endif
 
@@ -16,7 +15,7 @@ let package = Package(
     name: "HelloTuist",
     dependencies: [
         // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.10.2"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
     ]
 )
